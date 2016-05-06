cf8da75175d907eac3d8de407f0a804654c30250
Carl Baccus <carl.baccus@nasa.gov>
2016-05-06 14:03:50 -0700

    Add FinalCalled Username (AUI)

--- PyCDR.2.py
+++ PyCDR.2.py
@@ -26,10 +26,10 @@ with open((sys.argv[1]), 'r') as infile, open((sys.argv[2]), 'w') as outfile:
     reader = csv.reader(infile)
     next(reader, None)  # skip the headers
     writer = csv.writer(outfile)
-    writer.writerow(['Date/Time', 'Duration', 'Calling Number', 'Called Number', 'Final Called Number'] )
+    writer.writerow(['Date/Time', 'Duration', 'Calling Number', 'Called Number', 'Final Called Number', 'finalCalledPartyUnicodeLoginUserID'] )
     for row in reader:
         if row[47] == "0":
             pass
         else:
-            writer.writerow([date_and_time(row[47]),convert_duration(row[55]),row[8],row[29], row[30]])
+            writer.writerow([date_and_time(row[47]),convert_duration(row[55]),row[8],row[29], row[30], row[31]])
     print "All Done"
