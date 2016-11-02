ff26e5c8dd45b4c3b53580c1de04d5e22ab7c309
cjbaccus <cjbaccus@gmail.com>
2016-11-02 15:34:21 -0700

    Clean version

--- PyCDR.py
+++ PyCDR.py
@@ -5,6 +5,7 @@ import csv
 import time
 import re
 import xlsxwriter
+from datetime import datetime
  
 """
 PyCDR.py
@@ -14,9 +15,11 @@ Special thanks to original work done by Steve Campbell.
 This version is specifically catered to prepopulating an Excel spreadsheet.
 This version in particular is used for a very purpose specific means.
 """
-workbook = xlsxwriter.Workbook('CDRdata.xlsx')
+workbook = xlsxwriter.Workbook((sys.argv[2])+'.xlsx')
 worksheet = workbook.add_worksheet()
 
+# set formatting for date in excel
+format1 = workbook.add_format({'num_format': 'mm/dd/yy hh:mm:ss'})
  
 def date_and_time(time_value):
     return time.strftime("%m/%d/%y %H:%M:%S", time.localtime(float(time_value)))
@@ -27,27 +30,28 @@ def convert_duration(secs):
     h, m = divmod(m, 60)
     return "%d:%02d:%02d" % (h, m, s)
 
-with open((sys.argv[1]), 'r') as infile, open((sys.argv[2]), 'w') as outfile:
+with open((sys.argv[1]), 'r') as infile:
     reader = csv.reader(infile)
     next(reader, None)  # skip the headers
     xrow = 0
     xcol = 0
-    writer = csv.writer(outfile)
-    writer.writerow(['Date/Time', 'Duration', 'Calling Number', 'Called Number', 'Final Called Number', 'finalCalled-UserID'] )
-    # worksheet.write(A1, 'Date-Time', 'Duration', 'Calling Number', 'Called Number', 'Final Called Number', 'finalCalled-UserID')
+    worksheet.write(xrow, xcol, "Date-time")
+    worksheet.write(xrow, xcol + 1, "Duration")
+    worksheet.write(xrow, xcol + 2, "Calling Number")
+    worksheet.write(xrow, xcol + 3, "Called Number")
+    worksheet.write(xrow, xcol + 4, "Final Called Number")
+    worksheet.write(xrow, xcol + 5, "finalCalled-UserID")
+    xrow = 1
     for row in reader:
         if row[47] == "0":
             pass
         elif re.match("\d+" + "5000", row[29]):
-            # Just using 45000 for testing purposes.  Below worksheet lines will write each line into a column as it traverses row data.
-            worksheet.write(xrow, xcol, date_and_time(row[47]))
+            worksheet.write(xrow, xcol, date_and_time(row[47]), format1)
             worksheet.write(xrow, xcol + 1, convert_duration(row[55]))
             worksheet.write(xrow, xcol + 2, row[8])
             worksheet.write(xrow, xcol + 3, row[29])
             worksheet.write(xrow, xcol + 4, row[30])
             worksheet.write(xrow, xcol + 5, row[31])
             xrow += 1
-            # Following line is for writing to csv to test, commenting out for this branch.
-            # writer.writerow([date_and_time(row[47]),convert_duration(row[55]),row[8],row[29], row[30], row[31]])
     print "All Done"
 workbook.close()
